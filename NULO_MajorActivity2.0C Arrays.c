bb738ac698fc48d074a13dd855f2d4c33cadb018
CharlotteAnneNulo-T116 <102658202+CharlotteAnneNulo-T116@users.noreply.github.com>
2022-03-30 18:08:18 +0800

    Add files via upload

---
+++ NULO_MajorActivity2.0C Arrays.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int inf(int crdtls [3][2], int ID, int PIN)
+{
+    int temp = 0;
+    for(int i = 0; i<3; i++){
+        if(ID == crdtls[i][0] && PIN == crdtls[i][1]){
+            temp = 1;
+        }
+    }
+    return temp;
+}
+
+int main ()
+{
+    int crdtls[3][2] = {{132,564}, {756,891}, {432,567}};
+    int ID, PIN;
+
+    printf("Enter ID Number: \n");
+    scanf("%d", &ID);
+
+    printf("Enter PIN: \n");
+    scanf("%d", &PIN);
+
+    if(inf(crdtls,ID,PIN))
+    {
+        printf("You have successfully logged in!'");
+    } else {
+        printf("Invalid ID/PIN!\n ID#:%d", PIN);
+    }
+    return 0;
+}
